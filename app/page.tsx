09b38e602a7ce97d8d69292cee5297e73cd6ae49
sikkzz <sling0623@gmail.com>
2023-02-02 16:39:10 +0900

    Feat: post link routing, file system add

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import fs from 'fs'
+import Link from 'next/link'
+
+const getPostMetadata = () => {
+    const folder = "posts/"
+    const files = fs.readdirSync(folder)
+    const markdownPosts = files.filter((file) => file.endsWith(".md"))
+    const slugs = markdownPosts.map((file) => file.replace(".md", ""))
+    return slugs
+}
+
 const HomePage = () => {
-    return <h1>Hello!</h1>
+    const postMetadata = getPostMetadata()
+    const postPreviews = postMetadata.map((slug) => (
+        <div>
+            <Link href={`/posts/${slug}`}>
+                <h2>{slug}</h2>
+            </Link>
+        </div>
+    ))
+    return <div>{postPreviews}</div>
 }
 
 export default HomePage;
